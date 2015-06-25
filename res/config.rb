7d326efa45760f95b732a20542543eaab7f9a504
Liam Kirsh <liam.kirsh@riseup.net>
2015-06-25 15:29:56 -0700

    added wikihow stuff

---
+++ res/config.rb
@@ -0,0 +1,11 @@
+http_path = "../"
+css_dir = "../assets/css"
+sass_dir = "../assets/sass"
+images_dir = "../assets/img"
+javascripts_dir = "../"
+
+line_comments = false
+
+preferred_syntax = :scss
+
+output_style = :compressed # by Compass.app
